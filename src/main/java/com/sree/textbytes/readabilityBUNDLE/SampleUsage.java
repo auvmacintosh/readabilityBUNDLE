ca99595605a1c20c66e47a359f89299e9a3ee0bc
auvmacintosh <120029790@qq.com>
2017-10-09 20:02:05 +0800

    some try

--- src/main/java/com/sree/textbytes/readabilityBUNDLE/SampleUsage.java
+++ src/main/java/com/sree/textbytes/readabilityBUNDLE/SampleUsage.java
@@ -1,5 +1,8 @@
 package com.sree.textbytes.readabilityBUNDLE;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 import com.sree.textbytes.network.HtmlFetcher;
 
 
@@ -8,14 +11,29 @@ public class SampleUsage {
 		Article article = new Article();
 		ContentExtractor ce = new ContentExtractor();
 		HtmlFetcher htmlFetcher = new HtmlFetcher();
-		String html = htmlFetcher.getHtml("http://www.firstpost.com/tech/htc-eyes-15-indian-smartphone-market-to-open-7000-outlets-1164045.html", 0);
+		String html = htmlFetcher.getHtml("https://github.com/auvmacintosh/readabilityBUNDLE", 1000);
 		
 		//System.out.println("Html : "+html);
-		article = ce.extractContent(html, "ReadabilitySnack");
+		String cp = SampleUsage.class.getResource("/").getPath();
+		
+		article = ce.extractContent(readFileAsString(cp + "bwcx.html"), "ReadabilityGoose");
 		
 		System.out.println("Content : "+article.getCleanedArticleText());
 		
 		
 	}
 
+	public static String readFileAsString(String filePath) throws java.io.IOException {
+		StringBuilder fileData = new StringBuilder(1000);
+		BufferedReader reader = new BufferedReader(new FileReader(filePath));
+		char[] buf = new char[1024];
+		int numRead = 0;
+		while ((numRead = reader.read(buf)) != -1) {
+			String readData = String.valueOf(buf, 0, numRead);
+			fileData.append(readData);
+			buf = new char[1024];
+		}
+		reader.close();
+		return fileData.toString();
+	}
 }
